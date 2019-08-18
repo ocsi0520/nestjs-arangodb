9785ca14436889865e98a4b215ffdb2901fe9d18
Faragó Tamás <ocsi0520@gmail.com>
2019-08-18 13:29:53 +0200

    set tslint

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core'
+import {
+  FastifyAdapter,
+  NestFastifyApplication
+} from '@nestjs/platform-fastify'
+import { AppModule } from './app.module'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }))
+  await app.listen(3000)
+  // await app.listen(3000, '0.0.0.0'); // PROD: it only accepts localhost if we don't set '0.0.0.0'
 }
-bootstrap();
+bootstrap()
